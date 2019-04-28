b4bbb4ec103a1b8229360fb07a5c405936634fb3
Thanapon Phada-Anant <thanapon.p@clicknext.co.th>
2019-04-28 17:46:39 +0700

    add empty bad request and remove values controller

--- CustomerInquiry/Controllers/CustomerController.cs
+++ CustomerInquiry/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using CustomerInquiry.Models;
+using System;
 using System.Text.RegularExpressions;
 using System.Web.Http;
 
@@ -8,46 +9,54 @@ namespace CustomerInquiry.Controllers
     {
         public IHttpActionResult Post([FromBody]CustomerPostModel customerPost)
         {
-            int customerId = 0;
-            if (customerPost == null || (customerPost.CustomerID == null && customerPost.Email == null))
+            try
             {
-                return BadRequest("No inquiry criteria");
-            }
-
-            if(customerPost.Email != null)
-            {
-                if (!Regex.IsMatch(customerPost.Email, @"^[a-zA-Z0-9_.+-]+@[email]+\.[a-zA-Z0-9-.]+$"))
+                int customerId = 0;
+                if (customerPost == null || (customerPost.CustomerID == null && customerPost.Email == null))
                 {
-                    return BadRequest("Invalid Email");
-                }
-            }
-
-            if(customerPost.CustomerID != null)
-            {
-                if(!int.TryParse(customerPost.CustomerID, out customerId))
-                {
-                    return BadRequest("Invalid CustomerID");
+                    return BadRequest("No inquiry criteria");
                 }
 
-                if(customerId == 0)
+                if (customerPost.Email != null)
                 {
-                    return BadRequest("Invalid CustomerID");
+                    if (!Regex.IsMatch(customerPost.Email, @"^[a-zA-Z0-9_.+-]+@[email]+\.[a-zA-Z0-9-.]+$"))
+                    {
+                        return BadRequest("Invalid Email");
+                    }
                 }
+
+                if (customerPost.CustomerID != null)
+                {
+                    if (!int.TryParse(customerPost.CustomerID, out customerId))
+                    {
+                        return BadRequest("Invalid CustomerID");
+                    }
+
+                    if (customerId == 0)
+                    {
+                        return BadRequest("Invalid CustomerID");
+                    }
+                }
+
+                if (customerPost.Email != null && customerId != 0)
+                {
+
+                }
+                else if (customerPost.Email != null)
+                {
+
+                }
+                else
+                {
+
+                }
+
+                return Ok<CustomerModel>(new CustomerModel());
             }
-            
-            if(customerPost.Email != null && customerId != 0)
+            catch (Exception ex)
             {
-
+                return BadRequest("");
             }
-            else if (customerPost.Email != null)
-            {
-
-            } else
-            {
-
-            }
-
-            return Ok<CustomerModel>(new CustomerModel());
         }
     }
 }
