61ccee56a54a48f68b54172d5c22070da2529bb4
Thanapon Phada-Anant <thanapon.p@clicknext.co.th>
2019-04-28 16:00:50 +0700

    Initialize project with customer and trans model

---
+++ CustomerInquiry/Models/TransactionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerInquiry.Models
+{
+    public class TransactionModel
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public string Status { get; set; }
+    }
+}
