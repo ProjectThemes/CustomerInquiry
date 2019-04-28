7739b96352aa88cbcd77fcb9ff4c46198653a037
Thanapon Phada-Anant <thanapon.p@clicknext.co.th>
2019-04-28 22:06:39 +0700

    add date format to transaction date

---
+++ CustomerInquiry/Models/CustomerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using CustomerInquiry.Entities;
+
+namespace CustomerInquiry.Models
+{
+    public class CustomerFactory
+    {
+        public CustomerModel Create(Customer customer)
+        {
+            return new CustomerModel
+            {
+                CustomerID = customer.CustomerID,
+                Name = customer.CustomerName,
+                Email = customer.ContactEmail,
+                Mobile = customer.MobileNo,
+                Transactions = customer.Transactions.Take(5).OrderByDescending(t => t.TrasactionDate).Select(t => Create(t))
+            };
+        }
+
+        public TransactionModel Create(Transaction transaction)
+        {
+            return new TransactionModel
+            {
+                Id = transaction.TransactionID,
+                Date = transaction.TrasactionDate.ToString("dd/MM/yy HH:mm"),
+                Amount = transaction.Amount, //TODO 2 decimal place format
+                Currency = transaction.CurrencyCode,
+                Status = transaction.Status
+            };
+        }
+    }
+}
